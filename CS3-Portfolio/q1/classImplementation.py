931f5ff2520aae81677939529203acab1cfb6078
formavacua <ndgarra@brc.pshs.edu.ph>
2026-09-08 04:48:09 +0000

    new

---
+++ CS3-Portfolio/q1/classImplementation.py
@@ -0,0 +1,7 @@
+def __init__(VideoGame,GameName,Genre,Company,PlayerAu,Debug,Load,DLoad,Update,UnStall,Stall):
+    VideoGame.Name = GameName
+    VideoGame.Genre = Genre
+    VideoGame.__Debug = Debug
+    VideoGame.Company = Company
+    VideoGame.Player = PlayerAu
+    
