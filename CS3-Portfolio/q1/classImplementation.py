728257456e95a5585cb1bf0c0ed477603ae0ded7
formavacua <ndgarra@brc.pshs.edu.ph>
2026-09-13 07:45:53 +0800

    update
    Expanded analysis section with explanations for class design choices.

--- CS3-Portfolio/q1/classImplementation.py
+++ CS3-Portfolio/q1/classImplementation.py
@@ -45,7 +45,7 @@ print("Object state after update on object ")
 print(f"Object 1: {obj_1.Name} | Version: {obj_1.Update}")
 
 ## Analysis - 
-### Why did you make your chosen attribute private?
-### Which method changes the state of your object?
-### How did your two objects demonstrate that instances are independent?
-### What is the difference between your class diagram and your object diagram?
+### Why did you make your chosen attribute private? The visual clutter of lines of code shouldn't be seen by the user. Not only because the code can be vulnerable and dissectable for replicas, but also because the code would distract the player.
+### Which method changes the state of your object? The method 'update' updates the version of the game.
+### How did your two objects demonstrate that instances are independent? Object 2 wasn't affected by the update in object 1.
+### What is the difference between your class diagram and your object diagram? The class diagram shows what comprises the class-- the attributes and methods. While the object diagram shows the blueprint class being used to create objects.
